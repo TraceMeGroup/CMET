39d3e948bc14a8f4a9f0eaca0793f4ca7b5c6a6d
TraceMeGroup <traceme-zj@outlook.com>
2022-03-01 19:09:33 +0800

    TEST

--- CMETcore.py
+++ CMETcore.py
@@ -1,1 +1,13 @@
+import sys
+
 print("This is CMETcore.py")
+# get the preset values from run shell 
+arr_argv   = sys.argv
+runType    = str(arr_argv[1])
+modelNames = arr_argv[2]
+variable   = arr_argv[3]
+rangeYear  = arr_argv[4]
+month      = arr_argv[5]
+outType    = arr_argv[6]
+print(type(runType))
+# --------------------------------
